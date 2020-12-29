8bc406baadbcae36a854c450269d168bf0cddbaa
gougon <freddy2531@gmail.com>
2020-12-29 17:55:28 +0800

    add main function

--- grade/src/main/java/com/grade/App.java
+++ grade/src/main/java/com/grade/App.java
@@ -1,5 +1,9 @@
 package com.grade;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  * Hello world!
  */
@@ -12,7 +16,18 @@ public final class App {
      * @param args The arguments of the program.
      */
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.print("Enetr the score = ");
+        try {
+            InputStreamReader isr = new InputStreamReader(System.in);
+            BufferedReader br = new BufferedReader(isr);
+            int score = Integer.parseInt(br.readLine());
+            char grade = letterGrade(score);
+            System.out.println("The grade of " + score + " is " + grade);
+        } catch (NumberFormatException ex) {
+            System.out.println("Not an integer!");
+        } catch (IOException e) {
+            e.printStackTrace();
+        } 
     }
 
     public static char letterGrade(int score) {
